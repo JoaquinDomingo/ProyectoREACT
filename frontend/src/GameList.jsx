b1fa61e3cc0e707dc57e131f70dcd1bd45a5cf03
JoaquinDomingo <dojoaquindo@gmail.com>
2026-01-30 10:50:29 +0100

    Creación del componente para listar juegos

--- frontend/src/GameList.jsx
+++ frontend/src/GameList.jsx
@@ -3,4 +3,27 @@ import Game from "./Game";
 
 const GameList = ({games, onEdit, onDelete}) => {
     const [category, setCategory] = useState()
+    const filteredGames = games.filter(game => (category ? game.Categorias === category : true))
+
+    return (
+        <div>
+            <div className="filters">
+                <label>
+                    Filtrado por Categoria
+                    <input
+                        type="text"
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                    />
+                </label>
+            </div>
+            {filteredGames.map((game) => (
+                <Game key={game.id} game={game} onEdit={onEdit} onDelete={onDelete}/>
+            ))}
+        </div>
+    )
+
 }
+
+export default GameList
+
